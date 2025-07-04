8549b5a9f71c63379a221c4126b6b5044915de1d
Selenia Developer <seleniaprogramacion@gmail.com>
2025-07-04 13:59:38 -0300

    Add files via upload

---
+++ reason.js
@@ -0,0 +1,17 @@
+/*
+        var nombre,apellido,edad;
+
+        nombre=prompt("Ingrese su nombre");
+        apellido=prompt("Ingrese su apellido");
+        edad=Number(prompt("Ingrese su edad"));
+
+        alert("Buen dia, " + nombre + " " + apellido + " de " + edad + " anos de edad");
+
+*/
+
+
+
+
+
+        
+  
